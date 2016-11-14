35a490759597b955e010d08e21147059fef841b3
M.Jerger <michael.jerger@meissa-gmbh.de>
2016-11-14 14:27:02 +0100

    initially created project.clj

---
+++ project.clj
@@ -0,0 +1,27 @@
+(defproject org.domaindrivenarchitecture/dda-managed-ide "0.1.0-SNAPSHOT"
+  :description "module to install and configure ide based on ubuntu vm."
+  :url "https://www.domaindrivenarchitecture.org"
+  :pallet {:source-paths ["src"]}
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.palletops/pallet "0.8.12"]
+                 [com.palletops/stevedore "0.8.0-beta.7"]
+                 [org.domaindrivenarchitecture/dda-pallet-commons "0.2.0"]]
+  :profiles {:dev
+             {:dependencies
+              [[com.palletops/pallet "0.8.12" :classifier "tests"]
+               [org.domaindrivenarchitecture/dda-pallet-commons "0.2.0" :classifier "tests"]]
+              :plugins
+              [[com.palletops/pallet-lein "0.8.0-alpha.1"]
+               [lein-sub "0.3.0"]]}
+              :leiningen/reply
+               {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.21"]]
+                :exclusions [commons-logging]}}
+   :local-repo-classpath true
+   :repositories [["snapshots" :clojars]
+                  ["releases" :clojars]]
+   :deploy-repositories [["snapshots" :clojars]
+                         ["releases" :clojars]]
+   :classifiers {:tests {:source-paths ^:replace ["test"]
+                         :resource-paths ^:replace []}})
