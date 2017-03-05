b928f08ab850e421cf2763f90b06a59208ab1a0c
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-03-05 12:42:18 +0100

    removed ws

--- src/org/domaindrivenarchitecture/pallet/convention/managed_ide.clj
+++ src/org/domaindrivenarchitecture/pallet/convention/managed_ide.clj
@@ -46,12 +46,12 @@
 (s/defn default-ide-config :- crate/DdaIdeConfig
   "Managed vm crate default configuration"
   [user-key dev-platform vm-platform]
-  (map-utils/deep-merge 
+  (map-utils/deep-merge
     {:ide-user user-key
      :project-config dda-projects}
-    (cond 
+    (cond
       (= dev-platform :clojure-atom) {:clojure {:os-user-name (name user-key)}
-                                      :atom {:settings (if (= vm-platform :aws) 
+                                      :atom {:settings (if (= vm-platform :aws)
                                                          #{:install-aws-workaround}
                                                          #{})
                                              :plugins ["ink" "proto-repl"]}
@@ -59,15 +59,15 @@
       (= dev-platform :clojure-nightlight) {:clojure {:os-user-name (name user-key)
                                                       :settings #{:install-nightlight}}
                                             }
-      :default {})        
+      :default {})
     ))
 
 (s/defn ide-vm-config :- vm-crate/DdaVmConfig
   [user-key dev-platform vm-platform]
-  (cond 
+  (cond
     (= dev-platform :clojure-atom) (vm-convention/default-vm-config user-key vm-platform)
     (= dev-platform :clojure-nightlight) {:vm-user user-key
-                                          :settings #{:install-open-jdk-8 :install-linus-basics :install-git}})        
+                                          :settings #{:install-open-jdk-8 :install-linus-basics :install-git}})
   )
 
 (s/defn ^:always-validate ide-convention :- {:dda-managed-ide crate/DdaIdeConfig
@@ -76,7 +76,7 @@
   [convention-config :- DdaIdeConventionConfig]
   (let [user-key (:ide-user convention-config)
         vm-platform (:vm-platform convention-config)
-        dev-platform (:dev-platform convention-config)]  
+        dev-platform (:dev-platform convention-config)]
     {:dda-managed-ide (default-ide-config user-key dev-platform vm-platform)
      :dda-managed-vm (ide-vm-config user-key dev-platform vm-platform)
      :dda-backup (default-ide-backup-config user-key)}
