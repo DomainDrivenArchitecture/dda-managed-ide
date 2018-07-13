ba950b436918ee41159da75748927a87ac5df727
jem <michael.jerger@meissa-gmbh.de>
2018-07-13 08:57:26 +0200

    undo not existing compatibility

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-managed-ide "3.0.0-SNAPSHOT"
+(defproject dda/dda-managed-ide "2.0.0-SNAPSHOT"
   :description "module to install and configure ide based on ubuntu vm."
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
