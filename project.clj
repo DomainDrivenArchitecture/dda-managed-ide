47a12fc8b517393f2922065a9fc488a99c2027ee
jem <michael.jerger@meissa-gmbh.de>
2018-11-29 16:54:58 +0100

    Version 2.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-managed-ide "2.0.6"
+(defproject dda/dda-managed-ide "2.0.7-SNAPSHOT"
   :description "module to install and configure ide based on ubuntu vm."
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
