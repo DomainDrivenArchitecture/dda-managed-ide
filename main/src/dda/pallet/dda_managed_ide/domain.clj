47c7f78d763b8321dd2851d9dd4db994c94428fa
lukas <test-user@mydomain.org>
2018-12-28 13:25:03 +0100

    updated dbvis version

--- main/src/dda/pallet/dda_managed_ide/domain.clj
+++ main/src/dda/pallet/dda_managed_ide/domain.clj
@@ -116,7 +116,7 @@
        :basics {:argo-uml {:version "0.34"}
                 :yed {:download-url
                       "https://www.yworks.com/resources/yed/demo/yEd-3.18.2.zip"}
-                :dbvis {:version "10.0.15"}}}
+                :dbvis {:version "10.0.16"}}}
       (when (contains? ide-platform :atom)
         {:atom (atom/atom-config vm-type contains-clojure? contains-devops?)})
       (when (contains? ide-platform :idea)
