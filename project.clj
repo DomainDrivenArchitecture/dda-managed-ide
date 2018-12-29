49828df26cacf5023a2596508dcd75d0482860de
jem <michael.jerger@meissa-gmbh.de>
2018-12-29 11:34:38 +0100

    updated subc. lic.

--- project.clj
+++ project.clj
@@ -36,7 +36,8 @@
                        :main dda.pallet.dda-managed-ide.main
                        :uberjar-name "dda-managed-ide-standalone.jar"
                        :dependencies [[org.clojure/tools.cli "0.4.1"]
-                                      [ch.qos.logback/logback-classic "1.3.0-alpha4"]
+                                      [ch.qos.logback/logback-classic "1.3.0-alpha4"
+                                       :exclusions [com.sun.mail/javax.mail]]
                                       [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]}}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
