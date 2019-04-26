1140042c1b9b893607be3e6dfa10aae03d94fe18
jem <michael.jerger@meissa-gmbh.de>
2019-04-26 15:00:40 +0200

    introduce Target %s

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "2.2.2"]
+  :dependencies [[dda/dda-pallet "3.0.0-SNAPSHOT"]
                  [dda/dda-managed-vm "2.5.0-SNAPSHOT"]]
+  :target-path "target/%s/"
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -17,10 +18,10 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha3"]
-                    [dda/pallet "0.9.0" :classifier "tests"]
+                   [[org.clojure/test.check "0.10.0-alpha4"]
+                    [dda/pallet "0.9.1" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]
                    :plugins
                    [[lein-sub "0.3.0"]]
                    :leiningen/reply
@@ -29,16 +30,16 @@
                    :repl-options {:init-ns dda.pallet.dda-managed-ide.app.instantiate-existing}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.1" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-managed-ide.main
                        :uberjar-name "dda-managed-ide-standalone.jar"
-                       :dependencies [[org.clojure/tools.cli "0.4.1"]
+                       :dependencies [[org.clojure/tools.cli "0.4.2"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"
                                        :exclusions [com.sun.mail/javax.mail]]
-                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]}}
+                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]}}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
