7a0e69944446abedc360bdaa4e844f04b2c80260
lukas <test-user@mydomain.org>
2019-05-16 17:30:38 +0200

    Version 3.0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-managed-ide "3.0.5-SNAPSHOT"
+(defproject dda/dda-managed-ide "3.0.5"
   :description "module to install and configure ide based on ubuntu vm."
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
