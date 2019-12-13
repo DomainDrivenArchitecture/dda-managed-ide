3de24e812c10c8a031ddaf675c0a2a2c38091880
Ansgar Zwick <ansgar.zwick@meissa-gmbh.de>
2019-12-13 15:26:36 +0100

    update dependency dda-managed-vm to fix :touch error

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[dda/dda-pallet "3.0.0"]
-                 [dda/dda-managed-vm "2.6.2"]]
+                 [dda/dda-managed-vm "2.6.3-SNAPSHOT"]]
   :target-path "target/%s/"
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
